74ac8df75815744aaf746af9537a9f8b5c72a119
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-28 08:44:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData ( userName, userEmail){
+    const formData = {
+        name:userName,
+        email:userEmail
+    }
+
+    const config = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+           Accept : "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    }
+
+    return fetch("http://localhost:3000/users", config)
+    .then(response => response.json())
+    .then(json => {
+        let p = document.createElement('p');
+        p.textContent = json.id;
+        document.querySelector("body").appendChild(p);
+        console.log("id");
+    })
+
+    .catch((error) =>{
+        let p = document.createElement('p');
+        p.textContent = error.message;
+        document.querySelector("body").appendChild(p);
+    
+    });
+
+}
